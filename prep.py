1b691c17750a70557bc4e94c9ce45a453ee15587
soaxelbrooke <stuart@axelbrooke.com>
2017-09-30 14:21:29 +0900

    Add start token to vocab, pull review and summary length from config

--- prep.py
+++ prep.py
@@ -39,7 +39,7 @@ def review_iter():
         reviews = all_info['Reviews']
 
         if not (product_info.get('Price') or 'Unavailable').startswith('$'):
-            logging.warning("Skippig product {} due to price '{}'".format(
+            logging.warning("Skipping product {} due to price '{}'".format(
                 product_info['ProductID'], product_info['Price']))
             continue
 
@@ -57,14 +57,16 @@ def review_iter():
                 logging.warning('Skipping review {} due to {}'.format(review['ReviewID'], e))
 
 
-def reviews_to_x_y(enc, reviews):
-    # type: (Encoder, List[Dict[str, Union[int, str, float]]]) -> Tuple[ndarray, ndarray, ndarray]
+def reviews_to_x_y(model_cfg, enc, reviews):
+    # type: (ModelConfig, Encoder, List[Dict[str, Union[int, str, float]]]) -> Tuple[ndarray, ndarray, ndarray]
     """ Turns a review into the X and Y vecs, where product review content prefixed by product
         category, Y is a tuple of the review title, and the the overall score of the review.
     """
-    x = numpy.array(list(enc.transform([r['Content'] for r in reviews], fixed_length=100)),
+    x = numpy.array(list(enc.transform(['__' + r['Category'] + ' ' + r['Content'] for r in reviews],
+                                       fixed_length=model_cfg.review_len)),
                     dtype='int32')
-    y_title = numpy.array(list(enc.transform([r['Title'] for r in reviews], fixed_length=10)),
+    y_title = numpy.array(list(enc.transform([r['Title'] for r in reviews],
+                                             fixed_length=model_cfg.summary_len)),
                           dtype='int32')
     y_overall = numpy.array([[r['Overall']] for r in tqdm(reviews)], dtype='int32')
 
@@ -81,12 +83,12 @@ def prep(cfg: ModelConfig):
     random.shuffle(reviews)
 
     logging.info("Fitting BPE encoder with vocab size {}...".format(cfg.vocab_size))
-    encoder = Encoder(vocab_size=cfg.vocab_size, silent=False)
+    encoder = Encoder(vocab_size=cfg.vocab_size, silent=False, required_tokens=[cfg.start_token])
     encoder.fit([review['Content'] for review in tqdm(reviews)])
     encoder.save('current_encoder.json')
 
     logging.info("Encoding reviews and building x, y vectors...")
-    x, y_title, y_overall = reviews_to_x_y(encoder, reviews)
+    x, y_title, y_overall = reviews_to_x_y(cfg, encoder, reviews)
 
     logging.info("Saving X to ./data/x.csv...")
     numpy.savetxt('data/x.csv', x)
